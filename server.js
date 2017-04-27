de4e9773cf72575e04bf3a2c99e9f32f270a004a
System <none>
2017-04-27 14:50:44 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var app = express();
 var SpotifyWebApi = require('spotify-web-api-node');
 
 var redirectUri = 'https://spotify-easy-auth.glitch.me/callback';
+var tokenExpirationEpoch;
 
 var spotifyApi = new SpotifyWebApi({
   clientId : process.env.CLIENT_ID,
@@ -20,34 +21,22 @@ var spotifyApi = new SpotifyWebApi({
   redirectUri : redirectUri
 });
 
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    console.log('The access token expires in ' + data.body['expires_in']);
-    console.log('The access token is ' + data.body['access_token']);
+// spotifyApi.clientCredentialsGrant()
+//   .then(function(data) {
+//     console.log('The access token expires in ' + data.body['expires_in']);
+//     console.log('The access token is ' + data.body['access_token']);
 
-    // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong when retrieving an access token', err.message);
-  });
-
-var scopes = ['user-read-private', 'user-read-email'],
-    redirectUri = 'https://example.com/callback',
-    clientId = '5fe01282e44241328a84e7c5cc169165',
-    state = 'some-state-of-my-choice';
-
-// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-var spotifyApi = new SpotifyWebApi({
-  redirectUri : redirectUri,
-  clientId : clientId
-});
+//     // Save the access token so that it's used in future calls
+//     spotifyApi.setAccessToken(data.body['access_token']);
+//   }, function(err) {
+//     console.log('Something went wrong when retrieving an access token', err.message);
+//   });
 
 // Create the authorization URL
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+var authorizeURL = spotifyApi.createAuthorizeURL(['user-read-private']);
 
 // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
 console.log(authorizeURL);
-
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -55,6 +44,26 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/callback", function (request, response) {
+  var authorizationCode = request.query.code;
+  
+  response.sendFile(__dirname + '/views/index.html');
+  
+  spotifyApi.authorizationCodeGrant(authorizationCode)
+  .then(function(data) {
+
+    // Set the access token and refresh token
+    spotifyApi.setAccessToken(data.body['access_token']);
+    spotifyApi.setRefreshToken(data.body['refresh_token']);
+
+    // Save the amount of seconds until the access token expired
+    tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
+    console.log('Retrieved token. It expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+  }, function(err) {
+    console.log('Something went wrong when retrieving the access token!', err.message);
+  });
 });
 
 app.get("/search", function (request, response) {
