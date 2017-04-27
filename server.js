a212f6405d300486789ea59a90fa6cc229d34615
System <none>
2017-04-27 14:40:38 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ var app = express();
 
 var SpotifyWebApi = require('spotify-web-api-node');
 
+var redirectUri = 'https://spotify-easy-auth.glitch.me/callback';
+
 var spotifyApi = new SpotifyWebApi({
   clientId : process.env.CLIENT_ID,
   clientSecret : process.env.CLIENT_SECRET,
+  redirectUri : redirectUri
 });
 
 spotifyApi.clientCredentialsGrant()
@@ -27,6 +30,23 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err.message);
   });
+
+var scopes = ['user-read-private', 'user-read-email'],
+    redirectUri = 'https://example.com/callback',
+    clientId = '5fe01282e44241328a84e7c5cc169165',
+    state = 'some-state-of-my-choice';
+
+// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+var spotifyApi = new SpotifyWebApi({
+  redirectUri : redirectUri,
+  clientId : clientId
+});
+
+// Create the authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+// https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+console.log(authorizeURL);
 
 
 // http://expressjs.com/en/starter/static-files.html
