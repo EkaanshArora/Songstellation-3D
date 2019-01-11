9444a75dbd2270077e40f07a38fd39ce8b05776a
Glitch (spotify-oauth) <none>
2019-01-11 18:37:04 +0000

    👗🍖 Checkpoint ./server.js:2744428/29

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get("/callback", function (request, response) {
                             urilist.push(val.artists[0].id);
                             tracktitlelist.push(JSON.stringify(val.name));
                         }
-                        dataToSendObj = { 'message': data.body.items };
+                        dataToSendObj = { 'message': tracktitlelist };
                         console.log(urilist);
                         spotifyApi.getArtists(urilist)
                             .then(function (dataa) {
