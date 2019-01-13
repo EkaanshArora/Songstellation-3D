84f75aa21b6f33e5f841017a7e9797fd66bfe8a7
Glitch (spotify-oauth) <none>
2019-01-13 16:32:16 +0000

    🍐🙅 Checkpoint ./server.js:2744428/9

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/callback", function (request, response) {
                 tokenExpirationEpoch = (new Date().getTime() / 1000) + data.body['expires_in'];
                 console.log('Retrieved token. It expires in ' + Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
                 spotifyApi.getMyTopTracks({
-                    time_range: "short_term",
+                    time_range: "long_term",
                     limit: 10,
                     offset: 0
                 })
